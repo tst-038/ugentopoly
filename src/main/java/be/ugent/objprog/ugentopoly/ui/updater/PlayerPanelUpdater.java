41e4cd0eef9dd3d6b4536700f3136e724425d920
Tristan Degraeve <tristan.degraeve@gmail.com>
2024-05-03 10:30:42 +0200

    Dice roll button now disables on press

--- src/main/java/be/ugent/objprog/ugentopoly/ui/updater/PlayerPanelUpdater.java
+++ src/main/java/be/ugent/objprog/ugentopoly/ui/updater/PlayerPanelUpdater.java
@@ -75,6 +75,7 @@ public class PlayerPanelUpdater {
         Button rollDiceButton = (Button) playerNode.lookup("#rollDiceButton");
         rollDiceButton.setText(propertyReader.get("button.roll_dice"));
         rollDiceButton.setOnAction(event -> {
+            rollDiceButton.setDisable(true);
             gameManager.getTileInfoPaneManager().clearTileSelection();
             gameManager.getDiceHandler().rollDice(player);
         });
