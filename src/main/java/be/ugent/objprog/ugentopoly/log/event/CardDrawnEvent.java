4333fb323739739f96ad7f8a8a33a6c4b4831e43
Tristan Degraeve <tristan.degraeve@gmail.com>
2024-05-03 15:44:13 +0200

    updated card log.

--- src/main/java/be/ugent/objprog/ugentopoly/log/event/CardDrawnEvent.java
+++ src/main/java/be/ugent/objprog/ugentopoly/log/event/CardDrawnEvent.java
@@ -6,6 +6,6 @@ import be.ugent.objprog.ugentopoly.model.player.Player;
 public class CardDrawnEvent extends Event {
 
     public CardDrawnEvent(Player player, Card card) {
-        super(String.format(player.getGameManager().getPropertyreader().get("log.chance"), player.getName(), card.getDescription()));
+        super(String.format(player.getGameManager().getPropertyreader().get("log.cardDrawn"), player.getName(), card.getDescription()));
     }
 }
